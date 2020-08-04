e2f1f82c4169ca9b34d480a3452ad24af807e136
Markus Meier <maekke@gentoo.org>
2020-08-04 20:01:27 +0200

    split up openzh_covid19 function / logic

--- openzh_covid19.js
+++ openzh_covid19.js
@@ -1,38 +1,43 @@
+function getCantons() {
+	const cantons = [
+		"AG",
+		"AI",
+		"AR",
+		"BE",
+		"BL",
+		"BS",
+		"FR",
+		"GE",
+		"GL",
+		"GR",
+		"JU",
+		"LU",
+		"NE",
+		"NW",
+		"OW",
+		"SG",
+		"SH",
+		"SO",
+		"SZ",
+		"TG",
+		"TI",
+		"UR",
+		"VD",
+		"VS",
+		"ZG",
+		"ZH",
+		"FL",
+	];
+	return cantons;
+}
+
 function getOpenZhCovid19Data(data_column, callback) {
  	Plotly.d3.csv("https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv",
 		function(rows){
 			total_cases = [];
 			total_dates = [];
 
-			const cantons = [
-				"AG",
-				"AI",
-				"AR",
-				"BE",
-				"BL",
-				"BS",
-				"FR",
-				"GE",
-				"GL",
-				"GR",
-				"JU",
-				"LU",
-				"NE",
-				"NW",
-				"OW",
-				"SG",
-				"SH",
-				"SO",
-				"SZ",
-				"TG",
-				"TI",
-				"UR",
-				"VD",
-				"VS",
-				"ZG",
-				"ZH",
-				"FL",
-			];
+			const cantons = getCantons();
 
 			for (let i = 0; i < rows.length; ++i) {
 				row = rows[i];
@@ -54,38 +59,55 @@ function getOpenZhCovid19Data(data_column, callback) {
 					total_cases[d_index][c_index] = total_cases[d_index - 1][c_index];
 				}
 			}
-			console.log(total_cases)
 
-			total_cases_sum = [];
-			total_cases_sum.length = total_cases.length;
+			// "fix" unavailable data entries
+			total_cases_fixed = [];
+			total_cases_fixed.length = total_cases.length;
 			for (let i = 0; i < total_cases.length; ++i) {
-				total_cases_sum[i] = 0;
+				total_cases_fixed[i] = [];
+				total_cases_fixed[i].length = total_cases[i].length;
 				for (let j = 0; j < total_cases[i].length; ++j) {
 					if (total_cases[i][j] > 0) {
-						total_cases_sum[i] += total_cases[i][j];
+						total_cases_fixed[i][j] = total_cases[i][j];
 					}
 					else {
 						for (let x = i; x >= 0; --x) {
 							if (total_cases[x][j] > 0) {
-								total_cases_sum[i] += total_cases[x][j];
+								total_cases_fixed[i][j] = total_cases[x][j];
 								break;
 							}
 						}
 					}
 				}
 			}
-			console.log(total_cases_sum);
 
-			total_cases_inc = [];
-			total_cases_inc.length = total_cases_sum.length;
-			total_cases_inc[0] = 0;
-			for (let i = 1; i < total_cases_sum.length; ++i) {
-				total_cases_inc[i] = total_cases_sum[i] - total_cases_sum[i - 1];
-			}
-			console.log(total_cases_inc);
-
-			callback(total_dates, total_cases, total_cases_sum, total_cases_inc);
+			callback(total_dates, total_cases_fixed);
 		}
 	);
 }
 
+function sumCantonalData(data) {
+	data_sum = [];
+	data_sum.length = data.length;
+	for (let i = 0; i < data.length; ++i) {
+		data_sum[i] = 0;
+		for (let j = 0; j < data[i].length; ++j) {
+			if (data[i][j] > 0) {
+				data_sum[i] += data[i][j];
+			}
+		}
+	}
+	return data_sum;
+}
+
+function diffData(data) {
+	data_inc = [];
+	const n = data.length;
+	data_inc.length = n;
+	data_inc[0] = 0;
+	for (let i = 1; i < n; ++i) {
+		data_inc[i] = data[i] - data[i - 1];
+	}
+	data_inc[0] = data_inc[1] ;
+	return data_inc;
+}
