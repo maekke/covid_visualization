9e8790c53f318684acc31d96dde3ef605771fa77
Markus Meier <maekke@gentoo.org>
2020-11-28 21:44:00 +0100

    use NaNs in getOpenZhCovid19Data and provide fillUnavailableValues
    to have the "old" behavior.

--- openzh_covid19.js
+++ openzh_covid19.js
@@ -68,20 +68,23 @@ function getOpenZhCovid19Data(data_columns, callback) {
  	Plotly.d3.csv("https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv",
 		function(rows){
 			for (c = 0; c < data_columns.length; ++c) {
-				data_column = data_columns[c];
-				total_cases = [];
-				total_dates = [];
+				let data_column = data_columns[c];
+				let total_cases = [];
+				let total_dates = [];
 
 				const cantons = getCantons();
 
 				for (let i = 0; i < rows.length; ++i) {
-					row = rows[i];
+					let row = rows[i];
 					let d = row['date'];
 					let d_index = total_dates.indexOf(d)
 					if (d_index === -1) {
 						d_index = total_dates.push(d) - 1;
 						let x = []
 						x.length = cantons.length
+						for (let j = 0; j < x.length; ++j) {
+							x[j] = NaN;
+						}
 						total_cases.push(x)
 					}
 					let canton = row['abbreviation_canton_and_fl'];
@@ -90,45 +93,47 @@ function getOpenZhCovid19Data(data_columns, callback) {
 					if (numcul_confirmed !== "") {
 						total_cases[d_index][c_index] = parseInt(numcul_confirmed);
 					}
-					else if (numcul_confirmed === "" && d_index > 0) {
-						total_cases[d_index][c_index] = total_cases[d_index - 1][c_index];
+					else {
+						total_cases[d_index][c_index] = NaN;
 					}
 				}
-
-				// "fix" unavailable data entries
-				total_cases_fixed = [];
-				total_cases_fixed.length = total_cases.length;
-				for (let i = 0; i < total_cases.length; ++i) {
-					total_cases_fixed[i] = [];
-					total_cases_fixed[i].length = total_cases[i].length;
-					for (let j = 0; j < total_cases[i].length; ++j) {
-						if (total_cases[i][j] > 0) {
-							total_cases_fixed[i][j] = total_cases[i][j];
-						}
-						else {
-							for (let x = i; x >= 0; --x) {
-								if (total_cases[x][j] > 0) {
-									total_cases_fixed[i][j] = total_cases[x][j];
-									break;
-								}
-							}
-						}
-					}
-				}
-
-				callback(total_dates, total_cases_fixed, data_column);
+				callback(total_dates, total_cases, data_column);
 			}
 		}
 	);
 }
 
+function fillUnavailableValues(data) {
+	// fills NaN / unavailable with the last known value
+	let data_fixed = [];
+	data_fixed.length = data.length;
+	for (let i = 0; i < data.length; ++i) {
+		data_fixed[i] = [];
+		data_fixed[i].length = data[i].length;
+		for (let j = 0; j < data[i].length; ++j) {
+			if (data[i][j] > 0) {
+				data_fixed[i][j] = data[i][j];
+			}
+			else {
+				for (let x = i; x >= 0; --x) {
+					if (data[x][j] > 0) {
+						data_fixed[i][j] = data[x][j];
+						break;
+					}
+				}
+			}
+		}
+	}
+	return data_fixed;
+}
+
 function sumCantonalData(data) {
-	data_sum = [];
+	let data_sum = [];
 	data_sum.length = data.length;
 	for (let i = 0; i < data.length; ++i) {
 		data_sum[i] = 0;
 		for (let j = 0; j < data[i].length; ++j) {
-			if (data[i][j] > 0) {
+			if (!isNaN(data[i][j])) {
 				data_sum[i] += data[i][j];
 			}
 		}
@@ -137,12 +142,19 @@ function sumCantonalData(data) {
 }
 
 function diffData(data) {
-	data_inc = [];
+	let data_inc = [];
 	const n = data.length;
 	data_inc.length = n;
 	data_inc[0] = 0;
+	let last_val = data.length > 0 ? data[0] : 0;
 	for (let i = 1; i < n; ++i) {
-		data_inc[i] = data[i] - data[i - 1];
+		if (!isNaN(data[i])) {
+			data_inc[i] = data[i] - last_val;
+			last_val = data[i];
+		}
+		else {
+			data_inc[i] = NaN;
+		}
 	}
 	data_inc[0] = data_inc[1] ;
 	return data_inc;
